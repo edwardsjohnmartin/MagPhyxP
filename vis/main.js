3d5247228781444bc56320e143f80989d05d1fb1
John Edwards <edwardsjohnmartin@gmail.com>
2018-10-01 10:33:55 -0600

    visualization

--- vis/main.js
+++ vis/main.js
@@ -1,9 +1,16 @@
 dataset = [10, 15, 12, 18, 8];
 
-svg = d3.select("body")
-  .append("svg")
-  .attr("width", 300)
-  .attr("height", 220);
+// svg = d3.select("body")
+//   .append("svg")
+//   .attr("width", 300)
+//   .attr("height", 300);
+svg = d3.select("#svg");
+
+// svg2 = d3.select("body")
+//   .append("svg")
+//   .attr("width", 300)
+//   .attr("height", 300);
+svg2 = d3.select("#svg2");
 
 // let xScale = d3.scaleBand()
 //   .domain(dataset)
@@ -87,6 +94,62 @@ function energyChanged(i) {
   document.getElementById('energyLabel').innerHTML = (-0.33 + i*0.01).toFixed(2);
 }
 
+function updateVis2() {
+  // console.log(data);
+
+  minima = [];
+  data.forEach(function(d) {
+    let numBounces = d.numBounces;
+    console.log(numBounces);
+    d.energies.forEach(function(e) {
+      let energy = e.energy;
+      e.minima.forEach(function(m) {
+        let minimum = {
+          numBounces : numBounces,
+          energy : energy,
+          ptheta : m.ptheta,
+          pphi : m.pphi
+        };
+        minima.push(minimum);
+      });
+    });
+  });
+
+  console.log(minima);
+
+  // let xaccess = d => d.ptheta;
+  // let yaccess = d => d.pphi;
+
+  let eScale = d3.scaleLinear()
+    .domain([-0.33, -0.11])
+    .range([20, 480]);
+  // let xScale = d3.scaleLinear()
+  //   // .domain([d3.min(minima, xaccess), d3.max(minima, xaccess)])
+  //   .domain([-0.3, 0.3])
+  //   .range([20, 180]);
+  let yScale = d3.scaleLinear()
+    // .domain([d3.min(minima, yaccess), d3.max(minima, yaccess)])
+    .domain([-0.3, 0.3])
+    .range([20, 480]);
+
+  let color = d3.schemeCategory10;
+  let googleColors = ["#3366cc", "#dc3912", "#ff9900", "#109618", "#990099", "#0099c6", "#dd4477", "#66aa00", "#b82e2e", "#316395", "#994499", "#22aa99", "#aaaa11", "#6633cc", "#e67300", "#8b0707", "#651067", "#329262", "#5574a6", "#3b3eac"];
+    svg2.selectAll("circle")
+    .data(minima)
+    .enter()
+    .append("circle")
+    .attr("cx", function(d) { return eScale(d.energy); })
+    .attr("cy", function(d) { return yScale(d.pphi); })
+    .attr("title", d => `(${d.ptheta}, ${d.pphi})`)
+    // .attr("fill", d => color[-(d.energy*100) % 10])
+    // .attr("fill", d => color[d.numBounces])
+    .attr("fill", d => googleColors[d.numBounces])
+    .attr("stroke", 'none')
+    .attr("r", 3)
+  ;
+
+}
+
 function init() {
   // Load CSV file
   console.log('loading');
@@ -94,7 +157,9 @@ function init() {
     .then(function(d) {
       data = d;
       updateVis();
+      energyChanged(Number(document.getElementById('energySlider').value));
+      updateVis2();
     });
 
-  energyChanged(document.getElementById('energySlider').value);
+
 }
