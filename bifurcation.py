1227be0bbf8b58dabda8158cb7260c2c0dcea9d8
bojohnson02 <bo.johnson02@gmail.com>
2019-05-09 20:21:27 -0600

    cleaned up code

--- bifurcation.py
+++ bifurcation.py
@@ -1,57 +1,73 @@
+# -----------------------
+# Trace out the bifurcations for a given number of
+# bounces where energy starts at -1/3. When a
+# bifurcation is found, trace it out using a vector
+# and varying p_phi
+# -----------------------
+
+# Import needed libraries
 import magphyxp
 import numpy as np
+# -----------------------
+# Variables deciding whether
+# pphi is constant or ptheta
+# is constant
 VARY_PTHETA_PPHI = 0
 VARY_PTHETA_ENERGY = 1
+# -----------------------
 
-f = open('/home/bojohnson/Desktop/traces.txt','w')
+f = open('/home/bojohnson/Desktop/traces.txt','w') # File to write data to
 bifurcation_id = 0
-for num_bounces in [8]:
+for num_bounces in range(1,65):
+    # For each bounce number start the search
+    # with the given energy value E, the given v_step
+    # which is the vector step size in tracing out
+    # the bifurcation, and E_step which determines the
+    # step for E
     E = -1/3
-    previous_e = -1/3
-    previous_t = 0
+    previous_E = -1 # Placeholder for now
+    previous_t = -1 # Placeholder for now
     v_step = 1e-3
-    h = 1e-7
+    E_step = 1e-7
+    # ------------------------------------------------
     while (E <= 0):
-        m_one = magphyxp.calculate_min(1e-9, 1e-9, num_bounces, E, 1e-7,
-        VARY_PTHETA_ENERGY, 1e-7)
-        row = np.array([num_bounces,
-        np.sqrt(abs(2*m_one.energy + 2/3 - m_one.ptheta**2 - 10*m_one.pphi**2)),
-        m_one.ptheta,m_one.pphi,m_one.energy,m_one.t])
-        # if m.f < 1e-9 and m.energy > -1/3 and m.energy - previous_e > h and m.t - previous_t > 1 and m.t > 0:
-        if (m_one.f < 1e-9 and m_one.energy > -1/3 and
-        m_one.energy - previous_e > h and m_one.t > 0.0 and
-        m_one.t - previous_t > 1):
-            print('{} {} {:<.5f} {:<.5f} {:<.5f} {:<.5f} \
-            {} {} {:<.5f}\n'.format(bifurcation_id,num_bounces,
-            np.sqrt(abs(2*m_one.energy + 2/3 - m_one.ptheta**2 -
-            10*m_one.pphi**2)),m_one.ptheta,m_one.pphi,
-            m_one.energy,m_one.rocking_number/2,m_one.rocking_in_phase,m_one.t))
-            v = np.array((0,v_step,0))
-            s = np.array((row[2],row[3],row[4]))
-            while (s[2] < 0):
-                s_next = s + v
-                s_prev = s_next
-                # if (s_next[2] < s[2] or s_next[0] > s[0] or s_next[2] > 0):
-                if (s_next[1] > np.sqrt((s_next[2]+1/3-s_next[0]**2/2)/5)):
+        min_one = magphyxp.calculate_min(1e-9, 1e-9, num_bounces, E, 1e-7, VARY_PTHETA_ENERGY, 1e-7) # Calculate minimum
+        if (min_one.f < 1e-9 and min_one.energy > -1/3 and min_one.energy - previous_E > E_step and min_one.t > 0.0 and min_one.t - previous_t > 1):
+            vector = np.array((0, v_step, 0)) # Setup vector
+            curr_state = np.array((min_one.ptheta, min_one.pphi, min_one.energy)) # Get the current state of pphi, ptheta, and energy
+            epsilon = 1e-2 # Used in determining if the energy is increasing within some margin
+            new_bifurcation = True
+            while (curr_state[2] < 0):
+                next_state = curr_state + vector # Follow the vector to next state
+                prev_state = next_state # Save state where we came from
+                if (next_state[1] > np.sqrt((next_state[2] + 1/3 - next_state[0]**2 / 2) / 5) or next_state[2] > 0 or (next_state[2] + 1/3 - next_state[0]**2 / 2) / 5 < 0): # Make sure we're on the same trace
                     break
-                # print('a: {:<.10f} {:<.10f} {:<.10f}'.format(s_next[0],s_next[1],s_next[2]))
-                m = magphyxp.calculate_min(s_next[0], s_next[1], num_bounces,
-                s_next[2], 1e-7, VARY_PTHETA_ENERGY, 1e-7)
-                s_next = np.array((m.ptheta,m.pphi,m.energy))
-                v = s_next - s
-                s = s_next
-                # print('b: {:<.10f} {:<.10f} {:<.10f}'.format(s[0],s[1],s[2]))
-                # if (s_next[2] < s_prev[2]):
-                #     break
-                f.write('{} {} {:<.5f} {:<.5f} {:<.5f} {:<.5f} \
-                {} {} {:<.5f}\n'.format(bifurcation_id,num_bounces,
-                np.sqrt(abs(2*s[2] + 2/3 - s[0]**2 - 10*s[1]**2)),s[0],s[1],
-                s[2],m.rocking_number/2,m_one.rocking_in_phase,m.t))
-                f.flush
-                v[1] *= 1.1
-            previous_e = row[4]
-            bifurcation_id = bifurcation_id + 1
-        h = h*1.1
-        E = E+h
+                min_two = magphyxp.calculate_min(next_state[0], next_state[1], num_bounces, next_state[2], 1e-7, VARY_PTHETA_ENERGY, 1e-7) # Calculate new min at new location
+                if min_two.f > 1e-9:
+                    break
+                next_state = np.array((min_two.ptheta, min_two.pphi, min_two.energy)) # New min gives new state
+                vector = next_state - curr_state # Find new vector
+                curr_state = next_state # Change current state to the newest one found
+                if (next_state[2] < (prev_state[2] - epsilon)): # Make sure energy is increasing within some tolerance
+                    break
+                # Write out ID, N, pr, ptheta, pphi, energy, rocking number, in phase, period
+                if new_bifurcation:
+                    new_bifurcation = False
+                    bifurcation_id += 1
+                f.write(
+                    '{} {} {:<.5f} {:<.5f} {:<.5f} {:<.5f} {} {} {:<.5f}\n'.format(
+                    bifurcation_id,num_bounces, np.sqrt(abs(2*curr_state[2] + 2/3 - curr_state[0]**2 - 10*curr_state[1]**2)), curr_state[0], curr_state[1],
+                    curr_state[2], int(min_one.rocking_number / 2), min_one.rocking_in_phase, min_two.t)
+                )
+                # f.flush()
+                # Slowly increase the step size of the vector
+                vector[1] *= 1.1
+                vector[2] *= 1.1
+                # -------------------------------------------
+                epsilon *= 0.9 # Slowly decrease tolerance of energy difference
+            # bifurcation_id += 1
+            previous_E = min_one.energy
+        E_step = E_step * 1.1
+        E = E + E_step
 
 f.close()
